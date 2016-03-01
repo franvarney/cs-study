4e2a23d9565fadec59e8637d9699a0410b4c3ca6
Fran Varney <fran@modulus.io>
2016-02-29 22:21:07 -0500

    Clean up helper functions

--- helpers.js
+++ helpers.js
@@ -1,10 +1,12 @@
+exports.swap = function (array, a, b) {
+  var temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+}
+
 function makeSortedArray() {
   var sorted = [];
-
-  for (var i = 65; i < 91; ++i) {
-    sorted.push(i);
-  }
-
+  for (var i = 65; i < 91; ++i) sorted.push(i);
   return sorted;
 }
 
@@ -14,27 +16,18 @@ exports.sorted = sortedArray;
 
 // Fisher-Yates shuffle
 function makeUnsortedArray() {
-  var newArray = sortedArray.slice();
-  var currentIndex = newArray.length, temp, randomIndex;
+  var arrayCopy = sortedArray.slice(); // copy
+  var currentIndex = arrayCopy.length;
 
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    temp = newArray[currentIndex];
-    newArray[currentIndex] = newArray[randomIndex];
-    newArray[randomIndex] = temp;
+  while (currentIndex) {
+    var randomIndex = Math.floor(Math.random() * currentIndex);
+    --currentIndex;
+    exports.swap(arrayCopy, currentIndex, randomIndex);
   }
 
-  return newArray;
+  return arrayCopy;
 }
 
 var unsortedArray = makeUnsortedArray();
 
 exports.unsorted = unsortedArray;
-
-exports.swap = function (array, val1, val2) {
-  var temp = array[val1];
-  array[val1] = array[val2];
-  array[val2] = temp;
-}
